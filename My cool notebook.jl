af6a10921a2c61890e3d5ca0f7d10db9b0222a30
b-dx2 <49596432+b-dx2@users.noreply.github.com>
2023-04-05 09:45:21 +0200

    Update My cool notebook.jl
    Include Inferenzstatistik-notebook

--- My cool notebook.jl
+++ My cool notebook.jl
@@ -1,267 +1,243 @@
 ### A Pluto.jl notebook ###
-# v0.12.18
+# v0.19.22
 
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 5b2ee40e-a2b8-11ea-0fef-c35fe6918860
-md"""
-# The tower of Hanoi
-
-The tower of hanoi is a famous puzzle.
-
-![setup of the tower of a hanoi](https://upload.wikimedia.org/wikipedia/commons/0/07/Tower_of_Hanoi.jpeg)
-
-The game consists of three rods with disks stacked on top of them. The puzzle will start with all disks in a stack on one of the rods (like in the picture). The goal is to move all the discs to a single stack on  different rod.
-
-To move the disks, you have to follow the following rules:
-
-* You can move only one disk at a time.
-* For each move, you have to take the upper disk from one of the stacks, and place it on top of another stack or empty rod.
-* You cannot place a larger disk on top of a smaller disk.
-
-This notebook will define a Julia implementation of the puzzle. It's up to you to write an algorithm that solves it.
-"""
-
-# ╔═╡ 95fbd0d2-a2b9-11ea-0682-fdf783251797
-md"""
-## Setting up the game pieces
- 
-What does a Julia implementation look like? We're not really interested in writing code that will manipulate physical disks. Our final goal is a function that will give us a _recipe_ to solve the tower of hanoi, which is just a list of moves to make. Because of that, we can use a lot of abstraction in our implementation, and keep the data structures as simple as possible.
-
-To start, we have to define some representation of the disks and the stacks. The disks have one important property, which is that they are ordered. We can use integers to represent them.
-"""
-
-# ╔═╡ 620d6834-a2ba-11ea-150a-2132bb54e4b3
-num_disks = 8
-
-# ╔═╡ 35ada214-a32c-11ea-0da3-d5d494b28467
-md"""(Side note: the number of disks is arbitrary. When testing your function, you may want to set it to 1 or 2 to start.)"""
-
-# ╔═╡ 7243cc8e-a2ba-11ea-3f29-99356f0cdcf4
-all_disks = 1:num_disks
-
-# ╔═╡ 7e1ba2ac-a2ba-11ea-0134-2f61ed75be18
-md"""
-A single stack can be represented as an array with all the disks in it. We will list them from top to bottom.
-"""
-
-# ╔═╡ 43781a52-a339-11ea-3803-e56e2d08aa83
-first_stack = collect(all_disks)
-
-# ╔═╡ b648ab70-a2ba-11ea-2dcc-55b630e44325
-md"""
-Now we have to make three of those. 
-"""
-
-# ╔═╡ 32f26f80-a2bb-11ea-0f2a-3fc631ada63d
-starting_stacks = [first_stack, [], []]
-
-# ╔═╡ e347f1de-a2bb-11ea-06e7-87cca6f2a240
-md"""
-## Defining the rules
-
-Now that we have our "game board", we can implement the rules.
-
-To start, we make two functions for states. A state of the game is just an array of stacks.
-
-We will define a function that checks if a state is okay according to the rules. To be legal, all the stacks should be in the correct order, so no larger disks on top of smaller disks. 
-
-Another good thing to check: no disks should have appeared or disappeared since we started!
-"""
-
-# ╔═╡ 512fa6d2-a2bd-11ea-3dbe-b935b536967b
-function islegal(stacks)
-	order_correct = all(issorted, stacks)
-	
-	# check if we use the same disk set that we started with
-	
-	disks_in_state = sort([disk for stack in stacks for disk in stack])
-	disks_complete = disks_in_state == all_disks
-	
-	order_correct && disks_complete
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
 end
 
-# ╔═╡ c56a5858-a2bd-11ea-1d96-77eaf5e74925
-md"""
-Another function for states: check if we are done! We can assume that we already checked if the state was legal. So we know that all the disks are there and they are ordered correctly.  To check if we are finished, we just need to check if the last stack contains all the disks.
-"""
+# ╔═╡ a61e79d0-b44d-11ed-0f75-c3c2abe12679
+using Distributions, PlutoUI, StatsPlots , Random
 
-# ╔═╡ d5cc41e8-a2bd-11ea-39c7-b7df8de6ae3e
-function iscomplete(stacks)
-	last(stacks) == all_disks
+# ╔═╡ 7aad9fe9-6e20-4116-9c06-279f6794370d
+using KernelDensity, Plots
+
+# ╔═╡ 0f455005-3900-4feb-9464-47a092e5a9dd
+@bind m Slider(80:120)
+
+# ╔═╡ 092f10fb-38c6-4a2c-baad-e542191495df
+@bind sd Slider(0.5:0.5:2)
+
+# ╔═╡ 521c8a2d-71e6-4b3c-84f4-8295815196c2
+d = Normal(m, sd)
+
+# ╔═╡ b5eff4c2-e59d-4712-84cd-02ad17681477
+n = 10
+
+# ╔═╡ 6d5312c6-88a1-4c52-8e01-19fb7d0be82a
+p = 0.5
+
+# ╔═╡ 637ee94b-6f99-4940-b628-3e7593c00226
+begin
+	plot(Binomial(n, p), lw=3, legend=false, fill=(0.1,0,:red))
+	title!("B($(n),$(p))")
+	# plot!(range(-z*sd+m, stop=z*sd+m, length=100), Normal(m, sd), lw=3, legend=false, fill=(0.2,0,:green))
+	#new_xticks2 = ([-sd+m,m, m, sd+m], ["-sd", "\$\\bar \\m\$", "sd"])
+	#vline!(new_xticks2[1], lw=3)
 end
 
-# ╔═╡ 53374f0e-a2c0-11ea-0c91-97474780721e
-md"""
-Now the only rules left to implement are the rules for moving disks. 
+# ╔═╡ 978f7bd6-bfb0-4dd7-a2ce-dfe7c76d43a4
+B1 = rand(Binomial(1, p),10)'
 
-We could implement this as another check on states, but it's easier to just write a legal `move` function. Your solution will specify moves for the `move` function, so this will be the only way that the stacks are actually manipulated. That way, we are sure that nothing fishy is happening.
+# ╔═╡ 6f2f1b70-0e2c-499b-b6ee-d02012fac06f
+@bind dice Select(["H" => "🍎", "T" => "🍉"])
 
-We will make our `move` function so that its input consists of a state of the game, and instructions for what to do. Its output will be the new state of the game.
+# ╔═╡ 27ba5d8b-2021-4f2a-af58-ebfa9e55b357
+replace(B1, "0"=>"H", "1"=>"T")
 
-So what should those instructions look like? It may seem intuitive to give a _disk_ that should be moved, but that's more than we need. After all, we are only allowed to take the top disk from one stack, and move it to the top of another. So we only have to say which _stacks_ we are moving between.
+# ╔═╡ f4df6d7e-8ecc-4cb5-a878-cf3e14fbf6c8
+B1
 
-(Note that the `move` function is okay with moving a larger disk on top of a smaller disk. We already implemented that restriction in `islegal`.)
-"""
+# ╔═╡ a736777e-a21a-4a78-8edf-addc3fc131fa
+Random walk
 
-# ╔═╡ e915394e-a2c0-11ea-0cd9-1df6fd3c7adf
-function move(stacks, source::Int, target::Int)
-	# check if the from stack if not empty
-	if isempty(stacks[source])
-		error("Error: attempted to move disk from empty stack")
-	end
-	
-	new_stacks = deepcopy(stacks)
-	
-	disk = popfirst!(new_stacks[source]) # take disk
-	pushfirst!(new_stacks[target], disk) # put on new stack
-	
-	return new_stacks
-end
-
-# ╔═╡ 87b2d164-a2c4-11ea-3095-916628943879
-md"""
-## Solving the problem
-
-We have implemented the game pieces and the rules, so you can start working on your solution.
-
-To do this, you can fill in the `solve(stacks)` function. This function should give a solution for the given `stacks`, by moving all the disks from stack 1 to stack 3.
-
-As output, `solve` should give a recipe, that tells us what to do. This recipe should be an array of moves. Each moves is a `(source, target)` tuple, specifying from which stack to which stack you should move.
-
-For example, it might look like this:
-"""
-
-# ╔═╡ 29b410cc-a329-11ea-202a-795b31ce5ad5
-function wrong_solution(stacks)::Array{Tuple{Int,Int}}
-	return [(1, 2), (2, 3), (2, 1), (1, 3)]
-end
-
-# ╔═╡ ea24e778-a32e-11ea-3f11-dbe9d36b1011
-md"""
-Now you can work on building an actual solution. Some tips:
-* `solve(stacks)` can keep track of the board if you want, but it doesn't have to.
-* The section below will actually run your moves, which is very useful for checking them.
-* If you want, you can change `num_disks` to 1 or 2. That can be a good starting point.
-"""
-
-# ╔═╡ 010dbdbc-a2c5-11ea-34c3-837eae17416f
-function solve(stacks)::Array{Tuple{Int,Int}}
-	
-	# what to do?
-	
-	return []
-end
-
-# ╔═╡ 3eb3c4c0-a2c5-11ea-0bcc-c9b52094f660
-md"""
-## Checking solutions
-
-This is where we can check a solution. We start with a function that takes our recipe and runs it.
-"""
-
-# ╔═╡ 4709db36-a327-11ea-13a3-bbfb18da84ce
-function run_solution(solver::Function, start = starting_stacks)
-	moves = solver(deepcopy(start)) # apply the solver
-	
-	all_states = Array{Any,1}(undef, length(moves) + 1)
-	all_states[1] = starting_stacks
-	
-	for (i, m) in enumerate(moves)
-		try
-			all_states[i + 1] = move(all_states[i], m[1], m[2])
-		catch
-			all_states[i + 1] = missing
-		end
-	end
-	
-	return all_states
-end
-
-# ╔═╡ 372824b4-a330-11ea-2f26-7b9a1ad018f1
-md"""
-You can use this function to see what your solution does.
-
-If `run_solution` tries to make an impossible move, it will give `missing` from that point onwards. Look at what happens in the `wrong_solution` version and compare it to the moves in `wrong_solution`.
-"""
-
-# ╔═╡ d2227b40-a329-11ea-105c-b585d5fcf970
-run_solution(wrong_solution)
-
-# ╔═╡ 9173b174-a327-11ea-3a69-9f7525f2e7b4
-run_solution(solve)
-
-# ╔═╡ bb5088ec-a330-11ea-2c41-6b8b92724b3b
-md"""
-Now that we have way to run a recipe, we can check if its output is correct. We will check if all the intermediate states are legal and the final state is the finished puzzle.
-"""
-
-# ╔═╡ 10fb1c56-a2c5-11ea-2a06-0d8c36bfa138
-function check_solution(solver::Function, start = starting_stacks)
-	try
-		# run the solution
-		all_states = run_solution(solver, start)
-		
-		# check if each state is legal
-		all_legal = all(islegal, all_states)
-		
-		# check if the final state is is the completed puzzle
-		complete = (iscomplete ∘ last)(all_states)
-		
-		all_legal && complete
-	catch
-		# return false if we encountered an error
-		return false
-	end
-end
-
-# ╔═╡ 8ea7f944-a329-11ea-22cc-4dbd11ec0610
-check_solution(solve)
-
-# ╔═╡ e54add0a-a330-11ea-2eeb-1d42f552ba38
-if check_solution(solve)
-	if num_disks >= 8
-		md"""
-		Congratulations, your solution works!
-		"""
-	else
-		md"""
-		Your solution works for $(num_disks) disks. Change `num_disks` to see if it works for 8 or more.
-		"""
-	end
-else
-	md"""
-	The `solve` function doesn't work yet. Keep working on it!
+# ╔═╡ f4d18e96-904c-4742-9349-a42c3a76cde8
+begin
 	"""
+	generate n random walks from 0 to T with N steps
+	Returns:
+	   t (range) of time from 0 to T with N steps
+	   W (nxN array) of random walks
+	"""
+	function rwalks(T,N,n)
+	    dt = T/N
+	    dW = sqrt(dt)*randn(n,N)
+	    W = zeros(n,N)
+	    W[:,1] = dW[:,1]
+	    for j = 2:N
+	        W[:,j] .= W[:,j-1] .+ dW[:,j]
+	    end
+	    0:dt:T-dt, W
+	end
 end
+
+# ╔═╡ 933ad654-1ddf-4f80-af60-be82de10ff62
+# generate random walk
+t,W = rwalks(1,1000,10)
+
+# ╔═╡ eb11a282-0ea9-4235-9505-e3d56f97abcf
+begin
+	# convert random walk into DataFrames for plotting using Gadfly
+	using DataFrames
+	walk = convert(DataFrame, W')
+	dataname = names(walk) # [:x1, :x2, :x3, :x4, :x5, :x6, :x7, :x8, :x9, :x10]
+	walk[:t] = Array(t)
+	longform = stack(walk, dataname) # Gadfly prefer long form
+	
+	# plot action
+	using Gadfly
+	plot(longform, x=:t, y=:value, color=:variable, Geom.line, Guide.xlabel("t"), Guide.ylabel("W(t)"), Guide.title("random walk"))
+	
+	#=
+	DataFrams doc: <https://juliadata.github.io/DataFrames.jl/stable/index.html>
+	Gadfly doc: <http://gadflyjl.org/stable/index.html>
+	=#
+end
+
+# ╔═╡ 2bc06d73-cad4-4a5e-a541-873980971046
+md"""
+Wie viele Fälle sollen gezogen werden?
+"""
+
+# ╔═╡ 3f3675e7-6463-4c43-9bee-c985f78bacf8
+@bind n2 Slider(0:10, default=2)
+
+# ╔═╡ 6d6bd548-7ea0-4238-b549-c9dd3416965b
+td = truncated(d, m-5sd, m+5sd)
+
+# ╔═╡ 6bd524ba-54ed-4af2-9ab1-b2cd15e42b9d
+rand(td, n)'
+
+# ╔═╡ 0bdea5d9-9af5-48c9-af10-9bb43935dc4d
+
+
+# ╔═╡ 6ca2f007-e319-4ac1-8d2d-b5dfbc4552d0
+md"""
+Fläche unter einer Normalverteilung 
+"""
+
+# ╔═╡ 7d1c2280-e7b1-4dd0-8731-8f3a2e9978a9
+@bind z Slider(0:0.25:3, default=1)
+
+# ╔═╡ 92200bed-03c3-4959-b526-31a80ad418a2
+
+
+# ╔═╡ b981d3ab-7cd6-464b-bdfc-559f57f16da0
+
+
+# ╔═╡ b27b4d6a-447f-4df6-b760-e274c95702be
+import PlutoUI: combine
+
+# ╔═╡ 9438ad41-5177-4927-b86a-952e1df0bba4
+function wind_speed_input(directions::Vector)
+	
+	return combine() do Child
+		
+		inputs = [
+			md""" $(name): $(
+				Child(name, Slider(1:100))
+			)"""
+			
+			for name in directions
+		]
+		
+		md"""
+		#### Wind speeds
+		$(inputs)
+		"""
+	end
+end
+
+# ╔═╡ 2f3c0312-e899-4b6f-ba9d-2c4136e7e6fc
+@bind speeds wind_speed_input(["mean", "sd", "alpha-", "alpha+"])
+
+# ╔═╡ 956ab8df-bb77-4a31-b023-f86724098462
+md"""
+Der Mittelwert der Verteilung ist $(@bind m2 Scrubbable(50:1:150, default=100)) und die Standardabweichung $(@bind sd2 Scrubbable(5:1:20)). Das untere alpha ist $(@bind a1 Scrubbable(0.00:0.05:0.5, default=0.025, format=".3")), das obere $(@bind a2 Scrubbable(0.5:0.05:1, default=0.975, format=".3")),
+"""
+
+# ╔═╡ 6a06ee9b-4c80-4a49-b616-2fb3ebac9e6d
+function npdensity(z,inferior,superior)
+
+    n = size(z,2)
+    p = zeros(n)
+    for i = 1:n
+        x = z[:,i]
+        y = kde(x)
+        q05 = quantile(x,0.05)
+        Plots.plot(range(minimum(x), stop=inferior, length=100),z->pdf(y,z), color=:red, fill=(0,0.5,:red), label="-Zc")
+        q95 = quantile(x,0.95)
+        Plots.plot!(range(inferior, stop=superior, length=100),z->pdf(y,z), color=:green, fill=(0,0.5,:green),label="Região de Aceitação")
+        Plots.plot!(range(superior, stop=maximum(x), length=100),z->pdf(y,z), color = :red, fill=(0,0.5,:red),label="Zc")
+        m = mean(x)
+        Plots.plot!([m,m],[0,pdf(y,m)],color=:blue, label="média")
+        m = median(x)
+        if i == 1
+            p = Plots.plot!([m,m],[0,pdf(y,m)],color=:black, label="mediana")
+        else
+            p = [p, Plots.plot!([m,m],[0,pdf(y,m)],color=:black, label="mediana")]
+        end
+    end
+    return p
+end
+
+# ╔═╡ ac401b45-456d-4f7e-8c36-58f32086f38e
+parafusos = rand( Normal( 0 , 1 ),1000000)
+
+# ╔═╡ 3c03c053-02b2-42dc-8a5c-01114588ad13
+npdensity(parafusos,-1.96,1.96)
+
+# ╔═╡ 250cdbc4-bb19-4d93-a8c6-f689511b7974
+
+
+# ╔═╡ 02a1698a-2a4e-4e13-af4f-f3311d819559
+
+
+# ╔═╡ 08cb206d-cbb1-497c-a8a5-705dbc5bb05c
+@bind fruit Select(["apple" => "🍎", "melon" => "🍉"])
 
 # ╔═╡ Cell order:
-# ╟─5b2ee40e-a2b8-11ea-0fef-c35fe6918860
-# ╟─95fbd0d2-a2b9-11ea-0682-fdf783251797
-# ╠═620d6834-a2ba-11ea-150a-2132bb54e4b3
-# ╟─35ada214-a32c-11ea-0da3-d5d494b28467
-# ╠═7243cc8e-a2ba-11ea-3f29-99356f0cdcf4
-# ╟─7e1ba2ac-a2ba-11ea-0134-2f61ed75be18
-# ╠═43781a52-a339-11ea-3803-e56e2d08aa83
-# ╟─b648ab70-a2ba-11ea-2dcc-55b630e44325
-# ╠═32f26f80-a2bb-11ea-0f2a-3fc631ada63d
-# ╟─e347f1de-a2bb-11ea-06e7-87cca6f2a240
-# ╠═512fa6d2-a2bd-11ea-3dbe-b935b536967b
-# ╟─c56a5858-a2bd-11ea-1d96-77eaf5e74925
-# ╠═d5cc41e8-a2bd-11ea-39c7-b7df8de6ae3e
-# ╟─53374f0e-a2c0-11ea-0c91-97474780721e
-# ╠═e915394e-a2c0-11ea-0cd9-1df6fd3c7adf
-# ╟─87b2d164-a2c4-11ea-3095-916628943879
-# ╠═29b410cc-a329-11ea-202a-795b31ce5ad5
-# ╟─ea24e778-a32e-11ea-3f11-dbe9d36b1011
-# ╠═010dbdbc-a2c5-11ea-34c3-837eae17416f
-# ╟─3eb3c4c0-a2c5-11ea-0bcc-c9b52094f660
-# ╠═4709db36-a327-11ea-13a3-bbfb18da84ce
-# ╟─372824b4-a330-11ea-2f26-7b9a1ad018f1
-# ╠═d2227b40-a329-11ea-105c-b585d5fcf970
-# ╠═9173b174-a327-11ea-3a69-9f7525f2e7b4
-# ╟─bb5088ec-a330-11ea-2c41-6b8b92724b3b
-# ╠═10fb1c56-a2c5-11ea-2a06-0d8c36bfa138
-# ╠═8ea7f944-a329-11ea-22cc-4dbd11ec0610
-# ╟─e54add0a-a330-11ea-2eeb-1d42f552ba38
+# ╠═a61e79d0-b44d-11ed-0f75-c3c2abe12679
+# ╠═0f455005-3900-4feb-9464-47a092e5a9dd
+# ╠═092f10fb-38c6-4a2c-baad-e542191495df
+# ╠═521c8a2d-71e6-4b3c-84f4-8295815196c2
+# ╠═b5eff4c2-e59d-4712-84cd-02ad17681477
+# ╠═6d5312c6-88a1-4c52-8e01-19fb7d0be82a
+# ╠═637ee94b-6f99-4940-b628-3e7593c00226
+# ╠═978f7bd6-bfb0-4dd7-a2ce-dfe7c76d43a4
+# ╠═6f2f1b70-0e2c-499b-b6ee-d02012fac06f
+# ╠═27ba5d8b-2021-4f2a-af58-ebfa9e55b357
+# ╠═f4df6d7e-8ecc-4cb5-a878-cf3e14fbf6c8
+# ╠═a736777e-a21a-4a78-8edf-addc3fc131fa
+# ╠═f4d18e96-904c-4742-9349-a42c3a76cde8
+# ╠═933ad654-1ddf-4f80-af60-be82de10ff62
+# ╠═eb11a282-0ea9-4235-9505-e3d56f97abcf
+# ╠═2bc06d73-cad4-4a5e-a541-873980971046
+# ╠═3f3675e7-6463-4c43-9bee-c985f78bacf8
+# ╠═6d6bd548-7ea0-4238-b549-c9dd3416965b
+# ╠═6bd524ba-54ed-4af2-9ab1-b2cd15e42b9d
+# ╠═0bdea5d9-9af5-48c9-af10-9bb43935dc4d
+# ╟─6ca2f007-e319-4ac1-8d2d-b5dfbc4552d0
+# ╠═7d1c2280-e7b1-4dd0-8731-8f3a2e9978a9
+# ╠═a5d75065-83c6-4ae9-aab5-d275e9b1e50a
+# ╠═92200bed-03c3-4959-b526-31a80ad418a2
+# ╠═b981d3ab-7cd6-464b-bdfc-559f57f16da0
+# ╠═7aad9fe9-6e20-4116-9c06-279f6794370d
+# ╠═b27b4d6a-447f-4df6-b760-e274c95702be
+# ╠═9438ad41-5177-4927-b86a-952e1df0bba4
+# ╠═2f3c0312-e899-4b6f-ba9d-2c4136e7e6fc
+# ╠═956ab8df-bb77-4a31-b023-f86724098462
+# ╠═6a06ee9b-4c80-4a49-b616-2fb3ebac9e6d
+# ╟─ac401b45-456d-4f7e-8c36-58f32086f38e
+# ╠═3c03c053-02b2-42dc-8a5c-01114588ad13
+# ╠═250cdbc4-bb19-4d93-a8c6-f689511b7974
+# ╠═02a1698a-2a4e-4e13-af4f-f3311d819559
+# ╠═08cb206d-cbb1-497c-a8a5-705dbc5bb05c
